f31f55260bfad9807d981f5f7f5feea70d43da8f
Eric Miffre <eric.miffre@gmail.com>
2023-04-19 20:13:00 +0200

    fix: Fixed SWC dep

--- deps/any/swc.ts
+++ deps/any/swc.ts
@@ -1,5 +1,5 @@
 // @deno-types="npm:@swc/core@1.3.51"
-import swc from "npm:@swc/core@1.3.51"
+import * as swc from "npm:@swc/core@1.3.51"
 export { swc }
 
 export type * as SWC from "npm:@swc/core@1.3.51"
